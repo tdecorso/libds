51f2141d24ba8f921c246514fbe68ae9d9b0c586
tdecorso <tony.dcorso@gmail.com>
2026-03-01 15:10:12 +0100

    Library rework as header-only

---
+++ include/tds.h
@@ -0,0 +1,51 @@
+#ifndef H_TDS
+#define H_TDS
+
+#include <stdlib.h>
+
+typedef struct {
+    size_t count;
+    size_t capacity;
+} da_header_t;
+
+// Allocates header and data for the dynamic array
+#define da_alloc(ptr, cap)                                                         \
+    do {                                                                           \
+        da_header_t* header = malloc(sizeof(da_header_t) + cap * sizeof(*(ptr)));  \
+        header->count = 0;                                                         \
+        header->capacity = (cap);                                                  \
+        (ptr) = (void*) (header + 1);                                              \
+    } while (0);                                                                   \
+
+// Frees allocated memory for dynamic array
+#define da_free(ptr) \
+    do { \
+        free(da_header(ptr)); \
+    } while (0);
+
+// Pushes new item to the back of the array. Allocates new array if pointer is null.
+#define da_push_back(ptr, item) \
+    if (!(ptr)) da_alloc((ptr), 1); \
+    do { \
+        if (da_count(ptr) == da_capacity(ptr)) { \
+            da_header_t* header = da_header(ptr); \
+            size_t new_capacity = header->capacity * 2; \
+            size_t new_size = sizeof(da_header_t) + new_capacity * sizeof(*(ptr)); \
+            header = realloc(header, new_size);\
+            (ptr) = (void*) (header + 1); \
+            da_capacity(ptr) *= 2; \
+        } \
+        (ptr)[da_count(ptr)] = (item); \
+        da_count(ptr)++; \
+    } while (0); \
+
+// Returns pointer to dynamic array header
+#define da_header(ptr) ((da_header_t*) (ptr) - 1)
+
+// Current number of elements in the array
+#define da_count(ptr) (da_header(ptr)->count)
+
+// Current capacity of the array
+#define da_capacity(ptr) (da_header(ptr)->capacity)
+
+#endif // H_TDS
--- tests/test_da.c
+++ tests/test_da.c
@@ -1,224 +1,68 @@
+#include "tds.h"
+
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
 
-#include "tds/da.h"
+#define TEST(fun)                                              \
+    do {                                                       \
+        if ((fun)()) {printf("### Test failed.\n"); return 1;} \
+    } while (0);                                               \
 
-#define TEST(fun)                          \
-    do {                                   \
-        if ((fun)()) {                     \
-            printf("### Test failed.\n");  \
-            return 1;                      \
-        }                                  \
-    } while(0)
-
-bool test_invariant(da* arr) {
-    if (arr == NULL) return true;
-    if (arr->data == NULL) return true;
-    if (arr->capacity == 0) return true;
-    if (arr->element_size == 0) return true;
-    if (arr->count > arr->capacity) return true;
-    return false;
-}
 
 bool test_da_alloc() {
-    printf("### Testing alloc...\n");
 
-    size_t elem_size = sizeof(int);
-    size_t capacity = 5;
+    printf("### Testing da_alloc...\n");
 
-    da* arr = da_alloc(elem_size, capacity);
+    size_t capacity = 10;
+    int *numbers;
+    da_alloc(numbers, capacity);
 
-    if (test_invariant(arr)) return true;
+    if (numbers == NULL) return true;
+    if (sizeof(numbers[0]) != sizeof(int)) return true;
+    if (da_count(numbers) != 0) return true;
+    if (da_capacity(numbers) != capacity) return true;
 
-    if (arr->count != 0) return true;
-    if (arr->capacity != capacity) return true;
-    if (arr->element_size != elem_size) return true;
-
-    da_free(arr);
-
-    return false;
-}
-
-bool test_da_free() {
-    printf("### Testing free...\n");
-
-    da* arr = da_alloc(sizeof(int), 5);
-
-    if (test_invariant(arr)) return true;
-
-    da_free(arr);
-    arr = NULL;
+    da_free(numbers);
 
     return false;
 }
 
-bool test_da_append() {
-    printf("### Testing append...\n");
+bool test_da_push_back() {
 
-    da* arr = da_alloc(sizeof(int), 1);
+    printf("### Testing da_push_back...\n");
 
-    int values[3] = {1, 2, 3};
-    int results[3] = {0};
+    typedef struct {
+        int some;
+        float other;
+    } item_t;
 
-    da_append(arr, &values[0]);
-    da_append(arr, &values[1]);
-    da_append(arr, &values[2]);
+    item_t* items = NULL;
 
-    if (test_invariant(arr)) return true;
-    
-    memcpy(&results[0], arr->data, sizeof(int) * 3);
+    for (int i = 0; i < 5; ++i){
+        da_push_back(items, ((item_t){i, i+0.5f}));
+    }
 
-    if (arr->count != 3) return true;
-    if (arr->capacity != 4) return true;
-    if (results[0] != 1) return true;
-    if (results[1] != 2) return true;
-    if (results[2] != 3) return true;
-    
-    da_free(arr);
+    if (!items) return true;
+    if (da_count(items) != 5) return true;
+    if (da_capacity(items) != 8) return true;
 
-    return false;
-}
+    for (int i = 0; i < 5; ++i) {
+        if (items[i].some != i || items[i].other != i + 0.5f) return true;
+    }
 
-bool test_da_pop() {
-    printf("### Testing pop...\n");
-
-    da* arr = da_alloc(sizeof(int), 5);
-
-    int values[3] = {1, 2, 3};
-
-    da_append(arr, &values[0]);
-    da_append(arr, &values[1]);
-    da_append(arr, &values[2]);
-    
-    int results[3] = {0};
-    da_pop(arr, &results[0]);
-    da_pop(arr, &results[1]);
-    da_pop(arr, &results[2]);
-
-    if (test_invariant(arr)) return true;
-
-    if (results[0] != 3) return true;
-    if (results[1] != 2) return true;
-    if (results[2] != 1) return true;
-    if (arr->count != 0) return true;
-
-    da_free(arr);
-
-    return false;
-}
-
-bool test_da_get() {
-    printf("### Testing get...\n");
-
-    da* arr = da_alloc(sizeof(int), 5);
-
-    int values[3] = {1, 2, 3};
-
-    da_append(arr, &values[0]);
-    da_append(arr, &values[1]);
-    da_append(arr, &values[2]);
-    
-    int results[3] = {0};
-    da_get(arr, 0, &results[0]);
-    da_get(arr, 1, &results[1]);
-    da_get(arr, 2, &results[2]);
-
-    if (test_invariant(arr)) return true;
-
-    if (results[0] != 1) return true;
-    if (results[1] != 2) return true;
-    if (results[2] != 3) return true;
-
-    da_free(arr);
-
-    return false;
-}
-
-bool test_da_set() {
-    printf("### Testing set...\n");
-
-    da* arr = da_alloc(sizeof(int), 5);
-
-    int values[3] = {1, 2, 3};
-
-    da_append(arr, &values[0]);
-    da_append(arr, &values[1]);
-    da_append(arr, &values[2]);
-    
-    da_set(arr, 0, &values[2]);
-    da_set(arr, 1, &values[1]);
-    da_set(arr, 2, &values[0]);
-
-    int results[3] = {0};
-
-    da_get(arr, 0, &results[0]);
-    da_get(arr, 1, &results[1]);
-    da_get(arr, 2, &results[2]);
-
-    if (test_invariant(arr)) return true;
-
-    if (results[0] != 3) return true;
-    if (results[1] != 2) return true;
-    if (results[2] != 1) return true;
-
-    da_free(arr);
-
-    return false;
-}
-
-bool test_da_count() {
-    printf("### Testing count...\n");
-
-    da* arr = da_alloc(sizeof(int), 5);
-
-    int values[3] = {1, 2, 3};
-
-    da_append(arr, &values[0]);
-    da_append(arr, &values[1]);
-    da_append(arr, &values[2]);
-
-    if (test_invariant(arr)) return true;
-    
-    if (da_count(arr) != 3) return true;
-
-    da_free(arr);
-
-    return false;
-}
-
-bool test_da_capacity() {
-    printf("### Testing capacity...\n");
-
-    da* arr = da_alloc(sizeof(int), 1);
-
-    int values[3] = {1, 2, 3};
-
-    da_append(arr, &values[0]);
-    da_append(arr, &values[1]);
-    da_append(arr, &values[2]);
-
-    if (test_invariant(arr)) return true;
-    
-    if (da_capacity(arr) != 4) return true;
-    
-    da_free(arr);
+    da_free(items);
 
     return false;
 }
 
 int main(void) {
-    printf("\n### Testing dynamic array module...\n###\n");
+
+    printf("\n### Testing dynamic arrays...\n\n");
 
     TEST(test_da_alloc);
-    TEST(test_da_free);
-    TEST(test_da_append);
-    TEST(test_da_pop);
-    TEST(test_da_get);
-    TEST(test_da_set);
-    TEST(test_da_count);
-    TEST(test_da_capacity);
+    TEST(test_da_push_back);
 
-    printf("###\n### All tests passed. Bye! \n\n");
+    printf("\n### All tests passed. Bye!\n\n");
+
     return 0;
 }
